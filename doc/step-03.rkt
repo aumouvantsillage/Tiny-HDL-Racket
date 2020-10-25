7ad31420088116833d09e17ec5971142dd0db645
Guillaume Savaton <guillaume@baierouge.fr>
2020-10-25 19:02:08 +0100

    Step 3: added example

---
+++ doc/step-03.rkt
@@ -0,0 +1,7 @@
+Step 3: name resolution
+
+Then you can run the example by typing this command:
+
+```
+racket examples/full-adder-step-03-test.rkt
+```
---
+++ examples/full-adder-step-03-test.rkt
@@ -0,0 +1,4 @@
+#lang racket
+
+(require "full-adder-step-03.rkt")
+(include "full-adder-common-test.rkt")
