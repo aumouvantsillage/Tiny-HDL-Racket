10aaab5739e40657c711b0276265974881ae2340
Guillaume Savaton <guillaume@baierouge.fr>
2020-12-15 17:14:50 +0100

    Step 3: working version with built-in scoping APIs

--- lib/checker.rkt
+++ lib/checker.rkt
@@ -4,82 +4,108 @@
   "expander.rkt"
   (prefix-in stx/ "syntax.rkt")
   (for-syntax
+    threading
+    racket/function
     syntax/parse
-    "scope.rkt"))
+    syntax/strip-context
+    "scope.rkt"
+    (prefix-in meta/ "meta.rkt")))
 
 (provide begin-tiny-hdl)
 
 (define-syntax (begin-tiny-hdl stx)
-  (check (decorate stx)))
+  (replace-context stx ((checker stx))))
 
 (begin-for-syntax
-  (define (decorate stx)
-    (syntax-parse stx
-      #:literals [begin-tiny-hdl]
-
-      [(begin-tiny-hdl body ...)
-       (with-scope
-         #`(begin-tiny-hdl
-             #,@(map decorate (attribute body))))]
-
-      [:stx/entity
-       #:with port^ (with-scope (map decorate (attribute port)))
-       (bind! #'name
-         #'(entity name port^))]
-
-      [:stx/port
-       (bind! #'name stx)]
-
-      [:stx/architecture
-       #:with ent-name^   (add-scope #'ent-name)
-       #:with (body^ ...) (with-scope (map decorate (attribute body)))
-       (bind! #'name
-         #`(architecture name ent-name^ body^ ...))]
-
-      [:stx/instance
-       #:with arch-name^ (add-scope #'arch-name)
-       (bind! #'name
-         #'(instance name arch-name^))]
-
-      [:stx/assignment
-       #`(assign #,(decorate #'target) #,(decorate #'expr))]
-
-      [:stx/operation
-       #`(op #,@(map decorate (attribute arg)))]
-
-      [(inst-name:id port-name:id)
-       #:with inst-name^ (add-scope #'inst-name)
-       #'(inst-name^ port-name)]
-
-      [_ stx]))
+  (define (check-all lst)
+    (map (λ (f) (f)) lst))
 
   (define current-entity-name (make-parameter #f))
 
-  (define (check stx)
+  (define (checker stx)
     (syntax-parse stx
       #:literals [begin-tiny-hdl]
 
       [(begin-tiny-hdl body ...)
-       #`(begin
-           #,@(map check (attribute body)))]
+       (define body^ (with-scope
+                       (~>> (attribute body)
+                            (map add-scope)
+                            (map checker))))
+       (thunk
+         #`(begin
+             #,@(check-all body^)))]
+
+      [:stx/entity
+       ; An entity does not introduce a new scope.
+       ; Its ports are collected into a dictionary for later use.
+       (bind! #'name (meta/make-entity
+                       (for/hash ([p (in-list (attribute port))])
+                         (define/syntax-parse q:stx/port p)
+                         (values #'q.name (meta/port (syntax->datum #'q.mode))))))
+       (thunk stx)]
 
       [:stx/architecture
-       (parameterize ([current-entity-name #'ent-name])
-          #`(architecture name ent-name
-              #,@(map check (attribute body))))]
+       (bind! #'name (meta/architecture #'ent-name))
+       (define body^ (with-scope
+                       (~>> (attribute body)
+                            (map add-scope)
+                            (map checker))))
+       (thunk/in-scope
+         ; Check that ent-name refers to an entity.
+         (lookup #'ent-name meta/entity?)
+         ; Check the architecture body, providing the current entity name
+         ; as a parameter, for name resolution in port references.
+         (parameterize ([current-entity-name #'ent-name])
+           #`(architecture name ent-name
+               #,@(check-all body^))))]
+
+      [:stx/instance
+       (bind! #'name (meta/instance #'arch-name))
+       (thunk/in-scope
+         ; Check that arch-name refers to an architecture.
+         (lookup #'arch-name meta/architecture?)
+         stx)]
 
       [:stx/assignment
-       #`(assign #,(check #'target) #,(check #'expr))]
+       (define target^ (checker #'target))
+       (define expr^   (checker #'expr))
+       (thunk
+         #`(assign #,(target^) #,(expr^)))]
 
       [:stx/operation
-       #`(op #,@(map check (attribute arg)))]
+       (define arg^ (map checker (attribute arg)))
+       (thunk
+         #`(op #,@(check-all arg^)))]
 
       [(inst-name:id port-name:id)
-       (define/syntax-parse inst:stx/instance     (lookup #'inst-name))
-       (define/syntax-parse arch:stx/architecture (lookup #'inst.arch-name))
-       #`(port-ref arch.ent-name port-name inst-name)]
+       (thunk/in-scope
+         (define/syntax-parse ent-name
+           (~> #'inst-name
+               ; Check that inst-name refers to an instance.
+               (lookup meta/instance?)
+               ; Check that the architecture name for that instance refers to an architecture.
+               (meta/instance-arch-name)
+               (lookup meta/architecture?)
+               ; Get the entity name for that architecture
+               (meta/architecture-ent-name)))
+         (~> #'ent-name
+             ; Check that the entity name for that architecture refers to an entity.
+             (lookup meta/entity?)
+             ; Check that port-name is the name of a port in that entity.
+             (meta/entity-port-ref #'port-name))
+         ; Return a fully-resolved port-ref form.
+         #'(port-ref ent-name port-name inst-name))]
 
       [port-name:id
-       #`(port-ref #,(current-entity-name) port-name)]
+       (thunk/in-scope
+         ; Check that port-name refers to a port in the entity of the current architecture.
+         (define/syntax-parse ent-name (current-entity-name))
+         (~> #'ent-name
+             ; We already know that the current entity name refers to an entity.
+             (lookup)
+             ; Check that port-name is the name of a port in that entity.
+             (meta/entity-port-ref #'port-name))
+         ; Return a fully-resolved port-ref form.
+         #'(port-ref ent-name port-name))]
 
-      [_ stx])))
+      [_ (thunk stx)])))
--- lib/scope.rkt
+++ lib/scope.rkt
@@ -1,46 +1,38 @@
 #lang racket
 
-(require syntax/id-table)
+(require racket/function)
 
 (provide
   with-scope
-  bind!
+  thunk/in-scope
   add-scope
+  bind!
   lookup)
-
-; A scope has an optional parent scope
-; and a table that maps ids to custom data.
-(struct scope (parent table))
 
 (define current-scope (make-parameter #f))
 
-; Create a new scope with the given parent and an empty table.
-(define (make-scope [parent (current-scope)])
-  (scope parent (make-free-id-table)))
-
-; Introduce a new scope for the given body.
-(define-syntax-rule (with-scope body ...)
-  (parameterize ([current-scope (make-scope)])
+(define-syntax-rule (with-scope* sc body ...)
+  (parameterize ([current-scope sc])
     body ...))
 
-; Associate the given data to the given name in a scope.
-; Return the data unchanged.
-(define (bind! name data [sc (current-scope)])
-  (define table (scope-table sc))
-  (when (dict-has-key? table name)
-    (raise-syntax-error #f "Multiple bindings for identifier" name))
-  (dict-set! table name data)
-  data)
+(define-syntax-rule (with-scope body ...)
+  (with-scope* (syntax-local-make-definition-context (current-scope))
+    body ...))
 
-; Add scope information to a syntax object.
-(define (add-scope stx [sc (current-scope)])
-  (syntax-property stx 'scope sc))
+(define-syntax-rule (thunk/in-scope body ...)
+  (let ([sc (current-scope)])
+    (thunk (with-scope* sc body ...))))
 
-; Lookup a name in a scope chain. If no scope is specified,
-; start at the scope attached to the name.
-; Returns the corresponding data, or raise an error if not found.
-(define (lookup name [sc (syntax-property name 'scope)])
-  (unless sc
-    (raise-syntax-error #f "No declaration found for identifier" name))
-  (dict-ref (scope-table sc) name
-    (λ () (lookup name (scope-parent sc)))))
+(define (add-scope stx)
+  (internal-definition-context-introduce (current-scope) stx 'add))
+
+(define (bind! name data)
+  (syntax-local-bind-syntaxes (list name) #`'#,data (current-scope)))
+
+(define (lookup name [pred (λ (x) #t)])
+  (define res (syntax-local-value name
+                (λ () (raise-syntax-error #f "No declaration found for this identifier" name))
+                (current-scope)))
+  (unless (pred res)
+    (raise-syntax-error #f "Cannot be used in this context" name))
+  res)
